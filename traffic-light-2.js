30d646fae48cb78d602a7ed6534e2cbf72c06b05
mo92othman <mo92othman@gmail.com>
2023-08-19 17:27:30 +0200

    initial commit

---
+++ traffic-light-2.js
@@ -0,0 +1,19 @@
+const trafficLight = {
+  possibleStates: ["green", "orange", "red"],
+  stateIndex: 0,
+};
+
+let cycle = 0;
+while (cycle < 2) {
+  const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
+  console.log("The traffic light is on", currentState);
+
+  if (currentState === "green") {
+    trafficLight.stateIndex= 1;
+  } else if (currentState === "orange") {
+    trafficLight.stateIndex= 2;
+  } else if (currentState === "red") {
+    cycle++;
+    trafficLight.stateIndex= 0;
+  }
+}
